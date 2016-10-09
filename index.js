4eb02ae22ac80d4af4ebddf2e315c082f2211d27
Karim Alibhai <karim@alibhai.co>
2016-10-09 15:24:18 -0400

    add watching feature

--- index.js
+++ index.js
@@ -28,5 +28,12 @@ program.command('build [directory]').alias('b')
        .option('-s, --style [stylesheet]', 'use a custom stylesheet file')
        .action($( require('./lib/build') ))
 
+program.command('watch [directory]').alias('w')
+       .description('watches publication and compiles for web browser')
+       .option('-f, --format [types]', 'set the formats to build', list => list.split(',').reduce((a, b) => a && /^(pdf)$/i.test(b)), 'pdf')
+       .option('-t, --template [file]', 'use a custom ejs template file', __dirname + '/res/index.ejs')
+       .option('-s, --style [stylesheet]', 'use a custom stylesheet file')
+       .action($( require('./lib/watch') ))
+
 program.parse(process.argv)
 if (!caught) program.help()
